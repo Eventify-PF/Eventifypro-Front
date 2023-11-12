ffd33582d2d990934ac67f452ea5996c6a234ec6
Eventify-PF <149592550+Eventify-PF@users.noreply.github.com>
2023-11-12 18:30:57 -0300

    Update page.jsx

--- src/app/(pages)/event/[id]/page.jsx
+++ src/app/(pages)/event/[id]/page.jsx
@@ -1,8 +1,8 @@
 
 import EventDetail from "@/components/EventDetail";
 const loadEvent = async(id) =>{
-  //const res = await fetch(`https://server-eventifypro.onrender.com/events/${id}`);
-  const res = await fetch(`http://localhost:3001/events/${id}`);
+  const res = await fetch(`https://server-eventifypro.onrender.com/events/${id}`);
+  //const res = await fetch(`http://localhost:3001/events/${id}`);
   const data = await res.json();
   return data
 }
