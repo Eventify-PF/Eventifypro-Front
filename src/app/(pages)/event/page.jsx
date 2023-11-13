3cd51378b16527487d1383db6a475a0cfde61a28
FOteroRimini <facurimini@gmail.com>
2023-11-13 01:49:29 -0300

    borre importación sin usar

--- src/app/(pages)/event/page.jsx
+++ src/app/(pages)/event/page.jsx
@@ -1,11 +1,10 @@
-
 'use client';
+
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import Container from "@/components/Container";
 import { setCurrentPage } from "@/redux/action/eventActions";
 import EventCards from "@/components/EventCards/EventCards";
-import Filters from "@/components/Filters/Filters";
 import SearchBar from "@/components/SearchBar/SearchBar";
 import { fetchEvents, searchEvent, filterEventsByType, filterEventsByDate, filterEventsByTypeAndDate } from "@/redux/action/eventActions";
 import { getAllEventTypes } from "@/redux/action/eventTypeActions";
