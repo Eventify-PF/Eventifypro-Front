4fdbdf6aedfd220097b9cecdbb72314956feb1f9
Eventify-PF <149592550+Eventify-PF@users.noreply.github.com>
2023-11-15 19:09:13 -0300

    Update Review.jsx

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -40,7 +40,8 @@ const Review = () => {
     setError(false)
     try {
         const newComent = { comment, points, user } 
-        const {data} = await axios.post('http://localhost:3001/comments', newComent);
+        //const {data} = await axios.post('http://localhost:3001/comments', newComent);
+      const {data} = await axios.post('https://server-eventifypro.onrender.com/comments', newComent);
         dispatch(createReview({id: data.id, comment, points, user:activeUser.name}))
         setPoints(0);
         setRating(0);
@@ -52,7 +53,8 @@ const Review = () => {
 
   const deleteComment = async (id) => {
     try {
-      await axios.delete(`http://localhost:3001/comments/delet/${id}`)
+      //await axios.delete(`http://localhost:3001/comments/delet/${id}`)
+      await axios.delete(`https://server-eventifypro.onrender.com/comments/delet/${id}`)
       dispatch(deleteReview(id))
     } catch (error) {
       alert('Error Deleting This Task')
